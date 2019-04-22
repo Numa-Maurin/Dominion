c0f0d59c5a54e1a25df400266a3ac858fa3d6596
mathieu-LAGNY <mathieu.lagny@etu.umontpellier.fr>
2019-04-22 22:09:36 +0200

    maj playCard de Player Tous les tests de Player sont OK

--- src/test/java/fr/umontpellier/iut/dominion/GameTest.java
+++ src/test/java/fr/umontpellier/iut/dominion/GameTest.java
@@ -131,7 +131,7 @@ class GameTest {
         assertNull(minimalGame.removeFromSupply("Gold"));
     }
 
-    @Disabled
+
     @Test
     void testEndGame3Stack() {
         assertFalse(minimalGame.isFinished());
@@ -149,7 +149,7 @@ class GameTest {
         assertTrue(minimalGame.isFinished());
     }
 
-    @Disabled
+    
     @Test
     void testEndGameProvince() {
         for (int i = 0; i < 12; i++)
