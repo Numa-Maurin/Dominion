bf65212f0103447589851b56ba7a40b1ddce8419
Dorian66 <dorian.gerardin@gmail.com>
2019-04-22 22:19:17 +0200

    Carte bandit (marche pas)

--- src/main/java/fr/umontpellier/iut/dominion/cards/base/Bandit.java
+++ src/main/java/fr/umontpellier/iut/dominion/cards/base/Bandit.java
@@ -1,8 +1,16 @@
 package fr.umontpellier.iut.dominion.cards.base;
 
 import fr.umontpellier.iut.dominion.Game;
+import fr.umontpellier.iut.dominion.ListOfCards;
 import fr.umontpellier.iut.dominion.Player;
+import fr.umontpellier.iut.dominion.cards.Card;
+import fr.umontpellier.iut.dominion.cards.common.Gold;
+import fr.umontpellier.iut.dominion.cards.common.Silver;
 import fr.umontpellier.iut.dominion.cards.type.Attack;
+
+import java.util.ArrayList;
+
+import static fr.umontpellier.iut.dominion.CardType.Treasure;
 
 /**
  * Carte Bandit
@@ -17,10 +25,34 @@ public class Bandit extends Attack {
 
     @Override
     public void play(Player p) {
-       p.incrementMoney(1);
+        p.incrementMoney(1);
         for (Player o : p.getGame().otherPlayers(p)) {
-            for (int i = 0; i < 2; i++) {
-                o.drawCard();
+            Card revealCard1 = o.drawCard();
+            Card revealCard2 = o.drawCard();
+            if ((revealCard1.getTypes().contains(Treasure) & revealCard2.getTypes().contains(Treasure)) && ((revealCard1.getName() != "Copper" &
+                revealCard2.getName() != "Copper") && revealCard1.getName() != revealCard2.getName())) {
+                ListOfCards choice = new ListOfCards();
+                choice.add(revealCard1);
+                choice.add(revealCard2);
+                if (o.chooseCard("Choisissez la carte à écarter", choice, false).equals(revealCard1.getName())) {
+                    p.getGame().getTrashedCards().add(revealCard1);
+                    p.discardCard(revealCard2);
+                } else {
+                    p.getGame().getTrashedCards().add(revealCard2);
+                    p.discardCard(revealCard1);
+                }
+            }
+            else if (revealCard1.getTypes().contains(Treasure) && revealCard1.getName() != "Copper") {
+                p.getGame().getTrashedCards().add(revealCard1);
+                p.discardCard(revealCard2);
+            }
+            else if (revealCard2.getTypes().contains(Treasure) && revealCard2.getName() != "Copper") {
+                p.getGame().getTrashedCards().add(revealCard2);
+                p.discardCard(revealCard1);
+            }
+            else {
+                o.discardCard(revealCard1);
+                o.discardCard(revealCard2);
             }
         }
 
