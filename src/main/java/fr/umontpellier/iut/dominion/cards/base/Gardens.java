399abde7d3aecc633bd870d9cb588d68d1f4dfbc
mathieu-LAGNY <mathieu.lagny@etu.umontpellier.fr>
2019-04-23 17:28:10 +0200

    méthode getVictoryValue de Gardens

--- src/main/java/fr/umontpellier/iut/dominion/cards/base/Gardens.java
+++ src/main/java/fr/umontpellier/iut/dominion/cards/base/Gardens.java
@@ -1,5 +1,6 @@
 package fr.umontpellier.iut.dominion.cards.base;
 
+import fr.umontpellier.iut.dominion.Player;
 import fr.umontpellier.iut.dominion.cards.type.Victory;
 
 /**
@@ -10,5 +11,10 @@ import fr.umontpellier.iut.dominion.cards.type.Victory;
 public class Gardens extends Victory {
     public Gardens() {
         super("Gardens", 4);
+    }
+
+    @Override
+    public int getVictoryValue(Player p) {
+        return p.getAllCards().size()/10;
     }
 }
