fe5a4cce65761d688e169e0a9e7415d789013bd6
Dorian66 <dorian.gerardin@gmail.com>
2019-04-24 23:54:50 +0200

    Carte Harbinger fonctionnelle mais utilisation getDiscard()

--- src/main/java/fr/umontpellier/iut/dominion/cards/base/Harbinger.java
+++ src/main/java/fr/umontpellier/iut/dominion/cards/base/Harbinger.java
@@ -1,6 +1,11 @@
 package fr.umontpellier.iut.dominion.cards.base;
 
+import fr.umontpellier.iut.dominion.Player;
+import fr.umontpellier.iut.dominion.cards.Card;
 import fr.umontpellier.iut.dominion.cards.type.Action;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Carte Avant-coureur (Harbinger)
@@ -12,5 +17,24 @@ import fr.umontpellier.iut.dominion.cards.type.Action;
 public class Harbinger extends Action {
     public Harbinger() {
         super("Harbinger", 3);
+    }
+
+    @Override
+    public void play(Player p) {
+        p.incrementActions(1);
+        p.drawToHand();
+        List<String> choices = new ArrayList<>();
+        for (Card c : p.getDiscard()) {
+            choices.add(c.getName());
+        }
+        String scan = p.chooseOption("Quelle carte voulez-vous rajouter sur votre deck ?", choices, true);
+        if (scan.equals(""));
+        else {
+            p.addToDraw(p.getDiscard().getCard(scan));
+            p.getDiscard().remove(scan);
+        }
+
+
+
     }
 }
