bdb699ea3c517c17b44874a95ad9623b7ffd0a23
Numa MAURIN <numa.maurin@gmail.com>
2019-04-29 16:49:04 +0200

    debut Sentry

--- src/main/java/fr/umontpellier/iut/dominion/cards/base/Sentry.java
+++ src/main/java/fr/umontpellier/iut/dominion/cards/base/Sentry.java
@@ -1,5 +1,7 @@
 package fr.umontpellier.iut.dominion.cards.base;
 
+import fr.umontpellier.iut.dominion.Player;
+import fr.umontpellier.iut.dominion.cards.Card;
 import fr.umontpellier.iut.dominion.cards.type.Action;
 
 /**
@@ -9,9 +11,28 @@ import fr.umontpellier.iut.dominion.cards.type.Action;
  * +1 Action.
  * Regardez les 2 premières cartes de votre deck. Écartez et/ou défaussez celles que vous voulez.
  * Replacez les autres sur votre deck dans l'ordre de votre choix.
+ * Le joueur entre d’abord les noms des cartes qu’il souhaite écarter, et
+ * entre la chaîne vide pour passer à la suite. Il entre ensuite les noms des cartes
+ * qu’il souhaite défausser et passe avec la chaîne vide. Enfin, il entre les noms des
+ * cartes à replacer sur la pioche, la dernière étant replacée au-dessus.
  */
 public class Sentry extends Action {
     public Sentry() {
         super("Sentry", 5);
+    }
+
+    @Override
+    public void play(Player p) {
+        p.incrementActions(1);
+        p.drawToHand();
+        Card cartepiochee1 = p.drawCard();
+        Card cartepiochee2 = p.drawCard();
+        System.out.println(cartepiochee1.getName().toString());
+        System.out.println(cartepiochee2.getName().toString());
+
+    }
+
+
+
     }
 }
